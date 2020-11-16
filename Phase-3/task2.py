b33e27310747ee36c779037b27075baa015b6558
nithishb <nithish.mdh@gmail.com>
2020-11-15 21:09:03 -0700

    updates

--- Phase-3/task2.py
+++ Phase-3/task2.py
@@ -118,6 +118,189 @@ class Task2:
             final_result['acc']['voting'] = voting_acc/len(result)
             final_result['acc']['wt_scores'] = wt_scor_accuracy/len(result)
         json.dump(final_result, open(self.output_dir + "/{}_{}_dominant_{}_{}.txt".format(k, m, self.vm, self.uc), "w"), indent="\t")
+    
+    def decision_tree(self):
+        file = np.array(json.loads(json.load(open("phase2_outputs/task1/nmf_2_vectors.txt", "r"))))
+        print(file)
+        targets = {'vattene':0, 'combinato':1, 'daccordo':2}
+        key_list = list(targets.keys()) 
+        val_list = list(targets.values()) 
+        X = np.round(np.array([list(file[i]) for i in self.train_file_num_indices]), decimals=4)
+        labels = np.array([self.class_labels_map[k] for k in self.train_file_num])
+        y = np.array([targets[k] for k in labels])
+        clf = DecisionTreeClassifier(max_depth=4)
+        clf.fit(X, y)      
+        result = {}
+        for r in self.remove_indices_mat:
+            feat = list(file[r])
+            result[self.idx_file_map[r]] = {}
+            ypred = clf.predict([feat])
+            result[self.idx_file_map[r]]['predicted'] = key_list[val_list.index(ypred[0])]
+            result[self.idx_file_map[r]]['original'] = self.all_files_classes[self.idx_file_map[r]]
+        count=0
+        for k in result:
+            if result[k]['predicted'] == result[k]['original']:
+                count+=1
+        print("total", len(result))
+        print("correct", count)  
+        print("accuracy:", count/len(result))
+        json.dump(final_result, open(self.output_dir + "/decision_tree_{}_{}.txt".format(self.vm, self.uc), "w"), indent="\t")
+    
+    def knn(self):
+        def most_found(array):
+            list_of_words = []
+            for i in range(len(array)):
+                if array[i] not in list_of_words:
+                    list_of_words.append(array[i])
+            most_counted = ''
+            n_of_most_counted = None
+            for i in range(len(list_of_words)):
+                counted = array.count(list_of_words[i])
+                if n_of_most_counted == None:
+                    most_counted = list_of_words[i]
+                    n_of_most_counted = counted
+                elif n_of_most_counted < counted:
+                    most_counted = list_of_words[i]
+                    n_of_most_counted = counted
+                elif n_of_most_counted == counted:
+                    most_counted = None
+            return most_counted
+        
+        def find_neighbors(point, data, labels, k=3):
+            n_of_dimensions = len(point)
+            neighbors = []
+            neighbor_labels = []
+            for i in range(0, k):
+                nearest_neighbor_id = None
+                smallest_distance = None
+                for i in range(0, len(data)):
+                    eucledian_dist = 0
+                    for d in range(0, n_of_dimensions):
+                        dist = abs(point[d] - data[i][d])
+                        eucledian_dist += dist
+                    eucledian_dist = np.sqrt(eucledian_dist)
+                    if smallest_distance == None:
+                        smallest_distance = eucledian_dist
+                        nearest_neighbor_id = i
+                    elif smallest_distance > eucledian_dist:
+                        smallest_distance = eucledian_dist
+                        nearest_neighbor_id = i
+                neighbors.append(data[nearest_neighbor_id])
+                neighbor_labels.append(labels[nearest_neighbor_id])
+
+                data=np.delete(data,data[nearest_neighbor_id])
+                labels=np.delete(labels,labels[nearest_neighbor_id])
+            return neighbor_labels
+
+        def k_nearest_neighbor(point, data, labels, k=3):
+                # If two different labels are most found, continue to search for 1 more k
+            while True:
+                neighbor_labels = find_neighbors(point, data, labels, k=k)
+                label = most_found(neighbor_labels)
+                if label != None:
+                    break
+                k += 1
+                if k >= len(data):
+                    break
+            return label
+        
+        file = np.array(json.loads(json.load(open("phase2_outputs/task1/pca_2_vectors.txt", "r"))))
+        targets = {'vattene':0, 'combinato':1, 'daccordo':2}
+        key_list = list(targets.keys()) 
+        val_list = list(targets.values()) 
+        X = np.array([list(file[i]) for i in self.train_file_num_indices])
+        labels = np.array([self.class_labels_map[k] for k in self.train_file_num])
+        y = np.array([targets[k] for k in labels])       
+        result = {}
+        for r in self.remove_indices_mat:
+            feat = list(file[r])
+            result[self.idx_file_map[r]] = {}
+            ypred=k_nearest_neighbor(feat, X, y, k=5)
+            result[self.idx_file_map[r]]['predicted'] = key_list[val_list.index(ypred[0])]
+            result[self.idx_file_map[r]]['original'] = self.all_files_classes[self.idx_file_map[r]]
+        count=0
+        for k in result:
+            if result[k]['predicted'] == result[k]['original']:
+                count+=1
+        print("total", len(result))
+        print("correct", count)  
+        print("accuracy:", count/len(result))
+        json.dump(final_result, open(self.output_dir + "/knn_{}_{}.txt".format(self.vm, self.uc), "w"), indent="\t")
+    
+class Node:
+    def __init__(self, predicted_class):
+        self.predicted_class = predicted_class
+        self.feature_index = 0
+        self.threshold = 0
+        self.left = None
+        self.right = None
+        
+class DecisionTreeClassifier:
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+
+    def fit(self, X, y):
+        self.n_classes_ = len(set(y))
+        self.n_features_ = X.shape[1]
+        self.tree_ = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return [self._predict(inputs) for inputs in X]
+
+    def _best_split(self, X, y):
+        m = y.size
+        if m <= 1:
+            return None, None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes_)]
+        best_gini = 1.0 - sum((n / m) ** 2 for n in num_parent)
+        best_idx, best_thr = None, None
+        for idx in range(self.n_features_):
+            thresholds, classes = zip(*sorted(zip(X[:, idx], y)))
+            num_left = [0] * self.n_classes_
+            num_right = num_parent.copy()
+            for i in range(1, m):
+                c = classes[i - 1]
+                num_left[c] += 1
+                num_right[c] -= 1
+                gini_left = 1.0 - sum(
+                    (num_left[x] / i) ** 2 for x in range(self.n_classes_)
+                )
+                gini_right = 1.0 - sum(
+                    (num_right[x] / (m - i)) ** 2 for x in range(self.n_classes_)
+                )
+                gini = (i * gini_left + (m - i) * gini_right) / m
+                if thresholds[i] == thresholds[i - 1]:
+                    continue
+                if gini < best_gini:
+                    best_gini = gini
+                    best_idx = idx
+                    best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+        return best_idx, best_thr
+
+    def _grow_tree(self, X, y, depth=0):
+        num_samples_per_class = [np.sum(y == i) for i in range(self.n_classes_)]
+        predicted_class = np.argmax(num_samples_per_class)
+        node = Node(predicted_class=predicted_class)
+        if depth < self.max_depth:
+            idx, thr = self._best_split(X, y)
+            if idx is not None:
+                indices_left = X[:, idx] < thr
+                X_left, y_left = X[indices_left], y[indices_left]
+                X_right, y_right = X[~indices_left], y[~indices_left]
+                node.feature_index = idx
+                node.threshold = thr
+                node.left = self._grow_tree(X_left, y_left, depth + 1)
+                node.right = self._grow_tree(X_right, y_right, depth + 1)
+        return node
+
+    def _predict(self, inputs):
+        node = self.tree_
+        while node.left:
+            if inputs[node.feature_index] < node.threshold:
+                node = node.left
+            else:
+                node = node.right
+        return node.predicted_class
 
 if __name__ == "__main__":
     print("Performing Task 2")
